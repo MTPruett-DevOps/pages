965c21f9041f270b9c259ca9f967a01370d1180b
pruettmt <pruett.michaeltodd@gmail.com>
2025-04-13 17:24:59 -0500

    let's see

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Header from "./pages/header";
+import Header from "./header";
 import About from "./pages/about";
 import Blog from "./pages/blog";
 
