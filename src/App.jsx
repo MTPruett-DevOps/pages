5781147376ba25e83a6d830dba4f270aa2281a94
pruettmt <pruett.michaeltodd@gmail.com>
2025-04-15 22:17:58 -0500

    lets see

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Header from "./pages/header"
-import About from "./pages/about";
-import Docs from "./pages/docs";
+import Header from "./header"
+import About from "./about";
+import Docs from "./docs";
 
 export default function App() {
   return (
