d26ebfed0bd22eb7ab2d8a0efb9d215a57bb9fc3
pruettmt <pruett.michaeltodd@gmail.com>
2025-04-20 13:32:11 -0500

    lets see

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,8 @@
-// vite.config.mjs
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/pages/", // ← ✅ Set this to your repo name
   plugins: [react()],
   server: {
     port: 5173,
